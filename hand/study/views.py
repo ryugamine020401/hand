23955013ced33a687721c12745534be796caf54f
ymzk <sagamity0128@gmail.com>
2023-09-11 00:46:51 +0800

    TYPE:feat
    BODY
    - 修正影像辨識的function
    - 修改測驗一的template與post接收到的參數

--- hand/study/views.py
+++ hand/study/views.py
@@ -7,7 +7,6 @@ import random
 import numpy as np
 import mediapipe as mp
 import cv2
-from cvzone.HandTrackingModule import HandDetector
 from keras.models import load_model
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
@@ -99,7 +98,12 @@ def loging_check(func):
 # ------------------------- 登入驗證裝飾器 ------------------------------
 # ------------------------- 辨識 ------------------------------
 
-model = load_model("study/signDot.h5")
+model = load_model("study/signDot_with_z.h5")
+
+                # 正解
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# MODEL_FILE_PATH = os.path.join(BASE_DIR, 'static', 'models', 'signDot.h5')
+
 def num2alphabet(text):
     """
     將預測結果轉為英文字母
@@ -107,98 +111,78 @@ def num2alphabet(text):
     alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXY'
     return alphabet[text]
 
-                # 正解
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# MODEL_FILE_PATH = os.path.join(BASE_DIR, 'static', 'models', 'signDot.h5')
-
-def predict(img, correct):
+def hand_predict(img, correct):
     """
-    預測結果
+    辨識手的手勢
     """
-
-    detector = HandDetector(detectionCon=0.5, maxHands=1)  # cvzone,用於抓出手部位置
-    # model_file_path = os.path.join(os.path.dirname(__file__), 'static', 'models', 'signDot.h5')
-
+    correct = str(correct).upper()
     mp_hands = mp.solutions.hands  # mediapipe 偵測手掌方法
+
     hands_dot = mp_hands.Hands(
+        static_image_mode=True,
         model_complexity=1,  # 複雜度越高越準確，但會增加延遲
         max_num_hands=1,
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5)
 
-    hands = detector.findHands(img, draw=False)  # 使用cvzone抓出手部位置
-    if hands:  # 若畫面中有手，並成功偵測到
-        hand1 = hands[0]
-        img_x, img_y, img_w, img_h = hand1['bbox']  # 抓出手部座標
-        # print(x, y, w, h, sep=' ')
-        if img_w > img_h:  # 擷取手部的影像，並且確保擷取結果為爭方形，且不超出原圖範圍
-            img_y = int(img_y - (img_w - img_h) / 2)
-            img_h = img_w
-        else:
-            img_x = int(img_x - (img_h - img_w) / 2)
-            img_w = img_h
-        img = img[img_y - 20:img_y + img_h + 20, img_x - 20:img_x + img_w + 20]
-        if img_x - 20 > 0 and img_x + img_w + 20 < 765 and img_y - 20 > 0:
-            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # 轉RGB # pylint: disable=E1101
-            rgb_img = cv2.resize(rgb_img, (128, 128))  # 調整圖片大小 # pylint: disable=E1101
-            results = hands_dot.process(rgb_img)  # 偵測手部關節點座標
-            finger_points = []  # 記錄手指節點座標的陣列
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   #pylint: disable=E1101
+    results = hands_dot.process(rgb_img)
+    img_h, img_w, _ = img.shape
+    if results.multi_hand_landmarks:
+        for _, hand_coordinate in zip(results.multi_handedness, results.multi_hand_landmarks):
+            all_list = []
+            x_list = []
+            y_list = []
+            for _, point in enumerate(hand_coordinate.landmark):
+                # point_x, point_y, _ =
+                # int(point.img_x * img_w), int(point.img_y * img_h), int(point.img_z * img_w)
+                point_x, point_y = int(point.x * img_w), int(point.y * img_h)
+                all_list.append(point_x)
+                all_list.append(point_y)
+                all_list.append(point.z)
+                x_list.append(point_x)
+                y_list.append(point_y)
+            ## bbox
+            xmin, xmax = min(x_list), max(x_list)
+            ymin, ymax = min(y_list), max(y_list)
+            box_w, box_h = xmax - xmin, ymax - ymin
 
-            if results.multi_hand_landmarks:  # 若成功抓到座標
-                for hand_landmarks in results.multi_hand_landmarks:
-                    for i in hand_landmarks.landmark:
-                        # 將 21 個節點換算成座標，記錄到 finger_points
-                        finger_points.append(i.x)
-                        finger_points.append(i.y)
-                finger_points = np.array(finger_points)  # 轉為numpy array
-                finger_points = finger_points.reshape(1, 42)  # reshape
-                # print(finger_points.shape)
-                prediction = model.predict(finger_points, verbose=0)  # 輸出的是編碼
-                index = np.argmax(prediction)  # 將編碼轉換後才是結果
-                text = num2alphabet(index)
-                # return round(prediction.max(), 3), text
-                ans = {}
-                alphabets = 'ABCDEFGHIKLMNOPQRSTUVWXY'
-                count = 0
-                for alphabet in alphabets:
-                    ans[alphabet] = prediction[0][count]
-                    count = count + 1
-                response = {
-                    'result': text,
-                    'result_score': ans[text],
-                    'correct_score': ans[correct]
-                }
-                return response
+
+            if box_w > box_h:
+                ymin = int(ymin - (box_w - box_h) / 2)
+                box_h = box_w
             else:
-                # response = {
-                #     'result': False,
-                #     'result_score': ans[text],
-                #     'correct_score': ans[correct],
-                #     'msg' : 'mediapipe沒偵測到',
-                #     'detected' : False
-                # }
-                print('mediapipe沒偵測到')
-                return 'mediapipe沒偵測到'
-        else:
-            # response = {
-            #         'result': False,
-            #         'result_score': ans[text],
-            #         'correct_score': ans[correct],
-            #         'msg' : '手太靠近螢幕邊緣',
-            #         'detected' : False
-            #     }
-            print('手太靠近螢幕邊緣')
-            return '手太靠近螢幕邊緣'
+                xmin = int(xmin - (box_h - box_w) / 2)
+                box_w = box_h
+            for i in range(0, len(all_list), 3):
+                all_list[i] = (all_list[i] - xmin + 20) / (box_w + 40)
+            for j in range(1, len(all_list), 3):
+                all_list[j] = (all_list[j] - ymin + 20) / (box_h + 40)
+
+            all_list = np.array(all_list)  # 轉為numpy array
+            all_list = all_list.reshape(1, 63)  # reshape
+            prediction = model.predict(all_list, verbose=0)  # 輸出的是編碼
+            index = np.argmax(prediction)  # 將編碼轉換後才是結果
+            text = num2alphabet(index)
+            ans = {}
+            alphabets = 'ABCDEFGHIKLMNOPQRSTUVWXY'
+            count = 0
+            for alphabet in alphabets:
+                ans[alphabet] = prediction[0][count]
+                count = count + 1
+            payload = {
+                'result': text,
+                'result_score': ans[text],
+                'correct_score': ans[f'{correct}'],
+                'hand_exist' : True,
+            }
+            return payload
     else:
-        # response = {
-        #             'result': False,
-        #             'result_score': ans[text],
-        #             'correct_score': ans[correct],
-        #             'msg' : '沒手',
-        #             'detected' : False
-        #         }
-        print('no hands')
-        return 'no hands'
+        print('no hand in image')
+        payload = {
+                'hand_exist' : False,
+            }
+        return payload
 
 # ------------------------- 辨識 ------------------------------
 # ------------------------- test ------------------------------
@@ -263,11 +247,11 @@ class TestUploadImgView(APIView):
         # 將二進位資料轉換成NumPy陣列
         np_image = np.frombuffer(img.read(), np.uint8)
         image_array = cv2.imdecode(np_image, cv2.IMREAD_COLOR) # pylint: disable=E1101
-        image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB) # pylint: disable=E1101
+        # image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB) # pylint: disable=E1101
         print(image_array.size)
         result = aaa(img=image_array)
         print("結果一", result)
-        result = predict(img=image_array, correct='c')
+        result = hand_predict(img=image_array, correct='C')
         print("結果二", result)
         # print(np_image)
         # # 使用OpenCV讀取圖片
@@ -305,7 +289,7 @@ class UpLoadImgView(APIView):
         print(image_array.size)
         result = aaa(img=image_array)
         print("結果一", result)
-        result = predict(img=image_array, correct='c')
+        result = hand_predict(img=image_array, correct='c')
         print("結果二", result)
         # path = '/home/ymzk/桌面/HAND/hand/study/TEST/img.png'
         # cv2.imwrite(path, image_array) # pylint: disable=E1101
@@ -617,7 +601,8 @@ class TestOneViews(APIView):
         獲得頁面。
         """
         random_int = random.randint(1, 26)
-        alphabet = chr(TeachWordCard.objects.get(id=random_int).id)
+        alphabet = chr(TeachWordCard.objects.get(id=random_int).id+96)
+        # print((TeachWordCard.objects.get(id=random_int).id+96))
         response = Response(status=status.HTTP_202_ACCEPTED)
         payload = {
             "para1": param1,
@@ -653,23 +638,22 @@ class TestOneViews(APIView):
         """
         使用者送出圖片。
         """
-        # print(request.path, param1, param2)
-        # print(param2+1)
+        # print()
         encoded_image = request.data['image']
         # 從 Base64 編碼的字符串中解碼圖片數據
         decoded_image = base64.b64decode(encoded_image.split(',')[1])
         # 將二進制圖片數據轉換為 NumPy 數組
         image_array = np.frombuffer(decoded_image, dtype=np.uint8)
         image_array = cv2.imdecode(image_array, cv2.IMREAD_COLOR) # pylint: disable=E1101
-        image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB) # pylint: disable=E1101
-        # image_array = cv2.resize(image_array, (128, 128)) # pylint: disable=E1101
-        print(type(image_array))
+        # image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB) # pylint: disable=E1101
         cv2.imwrite("./img.png", image_array) # pylint: disable=E1101
-        print("儲存")
-        print(image_array.size)
-        result = aaa(img=image_array)
-        result = predict(img = image_array, correct = 'c')
+        result = hand_predict(img=image_array, correct=request.data['ans'])
+        if result['result_score'] == result['correct_score']:
+            print("手勢正確", result['result'])
         print(result)
-        result = False
-        return JsonResponse({'redirect_url' : f'../../{param1}/{param2+1}', 'detected':result})
+        payload = {
+            'redirect_url' : f'../../{param1}/{param2+1}',
+            'detected':result['hand_exist'],
+        }
+        return JsonResponse(payload)
 # ------------------------測驗_1------------------------------------
