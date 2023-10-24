89c0a03650e094364ac529a0a8f4c3c584ccc5e2
icgw <sagamity0239@gmail.com>
2023-10-24 23:20:24 +0800

    TYPE:docs
    - 移除不必要的區塊

--- backend/hand/onlinechat/views.py
+++ backend/hand/onlinechat/views.py
@@ -1,35 +1,27 @@
 """
-用來處理使用者引入字卡的時間
+用來處理return
 """
-# import datetime
-# import numpy as np
-# import mediapipe as mp
-# import cv2
-
-from django.shortcuts import render
 from django.http import JsonResponse
-# from rest_framework.authentication import get_authorization_header
 from rest_framework.views import APIView
 from rest_framework import status
-
-
-from reg.views import decode_access_token
-# from reg.models import UserIfm
 from onlinechat.models import OlineChatroom
-from reg.forms import LoginForm, EmailCheckForm
 
-# -------------- 聊天室 ---------------
-def lobby(request):
-    return render(request, 'lobby.html', {})
-# -------------- 聊天室 ---------------
+# # -------------- 聊天室 ---------------
+# def lobby(request):
+#     return render(request, 'lobby.html', {})
+# # -------------- 聊天室 ---------------
 
 # -------------- 聊天室獲取前幾筆聊天紀錄 ---------------
 class GetLeastChatAPIView(APIView):
+    """
+    獲得最後五筆的API
+    """
     def get(self, request):
-
-        last_record = OlineChatroom.objects.order_by('-id').last()
+        """
+        獲得最後五筆的API
+        """
         records = OlineChatroom.objects.order_by('id').reverse()[:5]
-        print(records)
+        print(records, request)
         messagelist = []
         for i in records[::-1]:
             data = {
